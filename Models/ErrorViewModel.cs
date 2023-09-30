1aceb7c4706a927f7f11fb6e86a2e9faa3f5b9dc
Antic20 <aleksandarantic.inf@gmail.com>
2023-09-30 15:13:14 +0200

    Commit
    Date 30.09.2023

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBankTransaction.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBankTransaction.Models
+{
+    public class Transaction
+    {
+        internal DateTime Date;
+
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage ="This field is rquired.")]
+        [MaxLength(12, ErrorMessage="Maximum 12 Characters only.")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This field is rquired.")]
+
+        public string BeneficiaryName { get; set; }
+       
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is rquired.")]
+
+        public string BankName { get; set; }
+        
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "This field is rquired.")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 Characters only.")]
+
+        public string SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "This field is rquired.")]
+        public int Amount { get; set; }
+
+        public DateTime Data  { get; set; }
+
+    }
+}
